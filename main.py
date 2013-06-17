2633fb545ee1c890d9c57683b6022e35c0bd360d
kylemsguy <kylezhou2002@hotmail.com>
2013-06-17 15:49:23 -0700

    Started implementing main.py. Command line arguments partially done

--- main.py
+++ main.py
@@ -3,4 +3,42 @@
 ##
 ## See the file license.txt for copying permission.
 
-## TODO: write main menu
+import os
+import sys
+
+def display_help():
+    print "Help shall be implemented soon"
+    exit()
+
+# Initialize variables
+vvvvvv_dir = None
+level_name = None
+script_file = None
+
+# Check for command line parameters
+args = sys.argv()
+if "--help" in args:
+    display_help()
+
+else:
+    skip = 0
+    for i in range(len(args)):
+        if skip:
+            skip -= 1
+            continue
+        
+        if args[i] == "-d":
+            vvvvvv_dir = args[i + 1]
+            skip = 1
+
+        elif args[i] == "-l":
+            level_name = args[i + 1]
+            skip = 1
+
+        elif args[i] == "-s":
+            script_file = args[i + 1].strip('"')
+            skip = 1
+
+        else:
+            print "invalid command line argument"
+            display_help()
